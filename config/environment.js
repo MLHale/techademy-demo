d447fd82924b6b2b08898346fdede562d411566b
mlhale <mlhale@unomaha.edu>
2016-06-12 21:11:39 -0700

    end of step 12

--- config/environment.js
+++ config/environment.js
@@ -2,21 +2,30 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'myproject',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
-
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+  modulePrefix: 'myproject',
+  environment: environment,
+  baseURL: '/',
+  locationType: 'auto',
+  EmberENV: {
+    FEATURES: {
+    // Here you can enable experimental features on an ember canary build
+    // e.g. 'with-controller': true
     }
+  },
+
+  APP: {
+    // Here you can pass flags/options to your application instance
+    // when it is created
+  },
+  contentSecurityPolicy: {
+    'default-src': "'self' ",
+    'script-src': "'self' ", // Allow scripts from self only, this prevents injection and XSS attacks
+    'font-src': "'self' http: https:", // Allow fonts to be loaded from self or any http/https source
+    'connect-src': "'self' ", // Allow data (ajax/websocket) from self only (The same IP as this app)
+    'img-src': "'self' http: https:", //Allow images from any http/https server
+    'style-src': "'self' ", // Allow inline styles and loaded CSS from same IP
+    'media-src': "'self' "
+  }
   };
 
   if (environment === 'development') {
